9a119089cadcff1783587c20ed2550586a12a373
cajgomez <Jgomez@cambridgeassociates.com>
2020-07-29 18:18:06 -0600

    Horizontal bouncing ball.

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # processing-3-kotlin
 Testing Processing3 with Kotlin
 
-#
+
+# Execute the main function in Class UsingProcessing2.kt
+
+```
+fun main() {
+    PApplet.main("UsingProcessing2")
+}
+```
--- src/main/kotlin/UsingProcessing2.kt
+++ src/main/kotlin/UsingProcessing2.kt
@@ -7,24 +7,37 @@ fun main() {
 class UsingProcessing2 : PApplet() {
     var circleX:Float = 0f
     var circleY:Float = 0f
+    var bounce:Boolean = false
 
     // identical use to draw in Prcessing IDE
     override fun draw() {
-        circleX = random(width.toFloat())
-        circleY = random(height.toFloat())
+
 
         background(50) // repaint the background every time
         ellipse(circleX, circleY, 24f, 24f)
 
-        circleX += 1
-     //   println(displayWidth)
+        if ( (circleX > width) && !bounce) {
+            bounce = true
+        }else if ( ( 0 > circleX ) && bounce) {
+            bounce = false
+        }
+
+        //   println(displayWidth)
+        if (bounce) {
+            circleX += -10
+
+        } else {
+            circleX += 10
+        }
+
 
     }
 
     // method for setting the size of the window
     override fun settings() {
         size(640, 360)
-
+        circleX = 300f//random(width.toFloat())
+        circleY = random(height.toFloat())
 
     }
 
